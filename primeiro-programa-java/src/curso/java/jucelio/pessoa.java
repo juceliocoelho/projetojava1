3901876da90b84e514ba32d98421db7c348a5db0
jucelio farias coelho <82785673+juceliocoelho@users.noreply.github.com>
2021-10-21 18:00:37 -0300

    datanascimento

--- primeiro-programa-java/src/curso/java/jucelio/pessoa.java
+++ primeiro-programa-java/src/curso/java/jucelio/pessoa.java
@@ -1,42 +1,49 @@
 package curso.java.jucelio;
 
+import java.util.Date;
+
 public class pessoa {
 
+	private String nome;
 
-		private String nome;
+	private String sobrenome;
 
-		private String sobrenome;
-		
-		
-		private int idade;
-		
-		
-
-		public int getIdade() {
-			return idade;
-		}
-
-		public void setIdade(int idade) {
-			this.idade = idade;
-		}
-
-		public String getNome() {
-			return nome;
-		}
-
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-
-		public String getSobrenome() {
-			return sobrenome;
-		}
-
-		public void setSobrenome(String sobrenome) {
-			this.sobrenome = sobrenome;
-		}
-		
-         
-		}
+	private int idade;
+	
+	private Date dataNascimento;
+	
 	
 
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+}
