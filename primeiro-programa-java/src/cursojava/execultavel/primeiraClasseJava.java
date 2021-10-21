844c785334ab49e842582e338d08ccf9f9abc593
jucelio farias coelho <82785673+juceliocoelho@users.noreply.github.com>
2021-10-21 16:41:45 -0300

    auteração

--- primeiro-programa-java/src/cursojava/execultavel/primeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/execultavel/primeiraClasseJava.java
@@ -6,7 +6,7 @@ public class primeiraClasseJava {
 	/*main é um auto execultavel em java */
 	public static void main(String[] args) {
 		 /* ESCREVE TEXTO NO CONSOLE*/
-		System.out.println(" Meu primeiro programa java");
+		System.out.println(" Meu primeiro programa java orientado a objeto");
 	}
 	
 }
