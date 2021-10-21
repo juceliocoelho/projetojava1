1e515c409542690adc2399e2559d1d43cbce8223
jucelio farias coelho <82785673+juceliocoelho@users.noreply.github.com>
2021-10-21 17:43:52 -0300

    idade

--- primeiro-programa-java/src/curso/java/jucelio/pessoa.java
+++ primeiro-programa-java/src/curso/java/jucelio/pessoa.java
@@ -6,6 +6,19 @@ public class pessoa {
 		private String nome;
 
 		private String sobrenome;
+		
+		
+		private int idade;
+		
+		
+
+		public int getIdade() {
+			return idade;
+		}
+
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
 
 		public String getNome() {
 			return nome;
