b1f511b10d66f30a517665d80ac0e9ef0d45074d
jucelio farias coelho <82785673+juceliocoelho@users.noreply.github.com>
2021-10-21 16:26:27 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/execultavel/primeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.execultavel;
+
+public class primeiraClasseJava {
+   
+	
+	/*main é um auto execultavel em java */
+	public static void main(String[] args) {
+		 /* ESCREVE TEXTO NO CONSOLE*/
+		System.out.println(" Meu primeiro programa java");
+	}
+	
+}
