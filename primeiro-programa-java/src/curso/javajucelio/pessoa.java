a85383a5737738efaca1f0f6e4146f51d1230ad9
jucelio farias coelho <82785673+juceliocoelho@users.noreply.github.com>
2021-10-21 17:33:04 -0300

    atulizaçao

---
+++ primeiro-programa-java/src/curso/javajucelio/pessoa.java
@@ -0,0 +1,18 @@
+package curso.javajucelio;
+
+public class pessoa {
+
+	public static void main(String[] args) {
+		private String nome;
+
+		private String sobrenome;
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+	}
+
+}
